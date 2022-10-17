089410c2235b8d3c3f1a64dda2c156c012c74279
hjlee <hjlee@DESKTOP-M7UCF0M>
2022-10-17 09:58:08 +0900

    주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -75,11 +75,25 @@ public class OrderApiController {
 				.collect(toList());
 		return result;
 	}
-
+	
+	/**
+	 * 주문 조회 V4: JPA에서 DTO 직접 조회
+	 */
+	
 	@GetMapping("/api/v4/orders")
 	public List<OrderQueryDto> ordersV4() {
 	 return orderQueryRepository.findOrderQueryDtos();
 	}
+	
+	/**
+	 * 주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화
+	 */
+	
+	@GetMapping("/api/v5/orders")
+	public List<OrderQueryDto> ordersV5() {
+	 return orderQueryRepository.findAllByDto_optimization();
+	}
+
 
 	
 	@Data
